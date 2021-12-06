7ebc847245374a1a3b5b5f0adc187c311c38e7fa
Victor Spirin <greatgomer3@gmail.com>
2021-12-06 22:01:20 +0300

    Add some architecture components

--- app/src/main/java/com/example/cats/utils/CatsService.kt
+++ app/src/main/java/com/example/cats/utils/CatsService.kt
@@ -1,15 +1,14 @@
-package com.example.cats
+package com.example.cats.utils
 
+import com.example.cats.CatModel
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://api.thecatapi.com/"
-
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(GsonConverterFactory.create())
-    .baseUrl(BASE_URL)
+    .baseUrl(Constants.BASE_URL)
     .build()
 
 interface CatsService {
