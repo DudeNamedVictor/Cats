9878b29be98d4a12e352132eb2e7178d1b3cd3d4
Victor Spirin <greatgomer3@gmail.com>
2021-12-06 22:20:54 +0300

    Revert commits

--- app/src/main/java/com/example/cats/CatsService.kt
+++ app/src/main/java/com/example/cats/CatsService.kt
@@ -5,11 +5,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://api.thecatapi.com/"
-
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(GsonConverterFactory.create())
-    .baseUrl(BASE_URL)
+    .baseUrl(Constants.BASE_URL)
     .build()
 
 interface CatsService {
