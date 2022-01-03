6c4dcbd44f50b0133bf7955489b1b3158bc4fd29
DudeNamedVictor <greatgomer3@gmail.com>
2022-01-04 01:46:09 +0300

    Fix main activity

--- app/src/main/java/com/example/cats/presentation/CatsActivity.kt
+++ app/src/main/java/com/example/cats/presentation/CatsActivity.kt
@@ -1,27 +1,29 @@
 package com.example.cats.presentation
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.example.cats.R
+import com.example.cats.databinding.CatsActivityLayoutBinding
 import com.google.android.material.tabs.TabLayout
 
 
 class CatsActivity : AppCompatActivity() {
+    private lateinit var binding: CatsActivityLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.cats_activity_layout)
 
-        val tabLayout = findViewById<View>(R.id.main_tabs) as TabLayout
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+        binding = CatsActivityLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainTabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
+                val navigation =
+                    Navigation.findNavController(this@CatsActivity, R.id.nav_host_fragment)
                 when (tab?.position) {
-                    0 -> Navigation.findNavController(this@CatsActivity, R.id.nav_host_fragment)
-                        .popBackStack()
-                    else -> Navigation.findNavController(this@CatsActivity, R.id.nav_host_fragment)
-                        .navigate(R.id.action_mainFragment_to_secondFragment)
+                    0 -> navigation.popBackStack(R.id.mainFragment, false)
+                    else -> navigation.navigate(R.id.action_mainFragment_to_secondFragment)
                 }
 
             }
